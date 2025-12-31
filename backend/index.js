e11d04b6b45d7ba70c1a3a619649267bd56309dd
Your Name <you@example.com>
2025-12-31 18:20:49 +0530

    addd

--- backend/index.js
+++ backend/index.js
@@ -14,4 +14,5 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected Successfully"))
     .catch(err => console.log("Mongo Error:", err));
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
